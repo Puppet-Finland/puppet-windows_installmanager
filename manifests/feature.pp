6c05d914596479936bae4d1c59c66c9315e924f2
Petri Lammi <petri.lammi@tietoteema.fi>
2018-05-07 09:10:00 +0300

    Add todo comment to use module hiera
    Signed-off-by: Petri Lammi <petri.lammi@tietoteema.fi>

---
+++ manifests/feature.pp
@@ -0,0 +1,23 @@
+define windows_snmp::feature
+(
+  $feature,
+)
+{
+  # TODO: Use module hiera
+  if ($facts['os']['release']['full'] in [ '7', '8.1', '10' ]) {
+    
+    dsc_windowsoptionalfeature { $feature:
+      dsc_ensure => 'Present',
+      dsc_name   => $feature,
+    }
+  }
+  elsif ($facts['os']['release']['full'] in [ '2003', '2008', '2008 R2', '2012', '2012 R2', '2016' ]) {
+    dsc_windowsfeature  { $feature:
+      dsc_ensure => 'Present',
+      dsc_name   => $feature,
+    }
+  }
+  else {
+    fail("Unsupported relase ${facts}['os']['release']")
+  }
+}
